2283be02a13d5a8b1b5b6172682b32208f2356a9
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-12 23:12:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
         end
-        if !final_array[key] = []
+        if !final_array[name][key] = []
           
           end
     end
