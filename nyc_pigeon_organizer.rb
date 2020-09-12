c85e61bda0a9112bb121b0939876dc19b0ef2ce3
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-12 22:52:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  new_pigeon_hash = {} 
   
-  data.count do |name, key, value|
+  hash.each_with_object({}) do |(k, v), a|
+ a[k] = v + 3
+end      
   
     
   end
